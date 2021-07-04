6bded8b62f5d9923bf82f6713d4f2fe3f1a141ff
liug1121 <61231796@qq.com>
2021-07-04 20:10:14 +0800

    commit

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -9,15 +9,6 @@ const getters = {
     getToken: state=>{
         return state.token;
     }
-    // getCardInfos: state => {
-    //     return state.cards;
-    // },
-    // getCardDetails: state=>{
-    //     return state.cardDetails;
-    // },
-    // getBindStatus: state=>{
-    //     return state.bindStatus
-    // }
 };
 
 const actions = {
@@ -32,37 +23,6 @@ const actions = {
         })
         
     }
-//   getCardInfos({commit}){
-//     api.getCardInfos(resp => {
-//       commit("setCardInfos", resp);
-//     }, null)
-//     },
-//     getCardDetails({commit}, queryParams){
-//         api.getCardDetails(resp => {
-//             commit("setCardDetails", resp);
-//         }, null,queryParams)
-//     },
-//     unbindCard({commit}, queryParams){
-//         api.unbindCard(resp=>{
-//             commit("setCardBindStatus", resp);
-//         }, null, queryParams)
-//     },
-//     bindCard({commit}, queryParams){
-//         return new Promise((resolve) => {
-//             api.bindCard(resp=>{
-//                 resolve(resp); 
-//                 commit("setBindResult", resp);
-//             }, null, queryParams)
-//         })
-//     },
-//     queryCardBindInfo({commit}, queryParams){
-//         return new Promise((resolve) => {
-//             api.queryCardBindInfo(resp=>{
-//                 resolve(resp); 
-//                 commit("setCardBindInfo", resp);
-//             }, null, queryParams)
-//         })
-//     }
 };
 
 const mutations = {
@@ -71,34 +31,6 @@ const mutations = {
             state.token = resp.data.data
         } 
     }
-    // setCardBindStatus(state, resp){
-    //     if(resp.data.resultCode == 0){
-    //         state.bindStatus = 0
-    //     }
-        
-    // },
-    // setCardInfos(state, resp) {
-    //     if(resp.data.resultCode == 0){
-    //         let cards=resp.data.data
-    //         state.cards = cards
-    //         if(state.cards.length == 0)
-    //             state.bindStatus = 0
-    //         else
-    //             state.bindStatus = 1
-    //     }
-    // },
-    // setCardDetails(state, resp){
-    //     if(resp.data.resultCode == 0){
-    //         let cardDetails=resp.data.data
-    //         state.cardDetails = cardDetails
-    //     }
-    // },
-    // setCardBindInfo(state, resp){
-    //     state.bindInfo = resp.data
-    // },
-    // setBindResult(state, resp){
-    //     state.bindResult = resp.data
-    // }
 };
 
 export default {
